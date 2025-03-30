0c200f04fc0235269e6f25a0dac6797d93574382
Addison Chan <addison.chan.kh@gmail.com>
2025-03-30 04:45:32 -0400

    Added unit tests for the Tile class

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Tile.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Tile.java
@@ -1,16 +1,11 @@
 package ca.mcmaster.se2aa4.mazerunner;
 import java.util.Map;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.util.HashMap;
 
 public class Tile {
     private final Position position;
     private final Type type;
     private static final Map<Position, Tile> TILES = new HashMap<>();
-    private static final Logger logger = LogManager.getLogger();
 
     private Tile(Position position, Type type){
         this.position = position;
@@ -19,7 +14,6 @@ public class Tile {
 
     public static Tile createTile(Position p, Type t){
         if(TILES.get(p) == null){
-            logger.info("WAS TRIGGERED");
             TILES.put(p,new Tile(p, t));
         }
         return TILES.get(p);
--- src/test/java/ca/mcmaster/se2aa4/mazerunner/TileTest.java
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/TileTest.java
@@ -15,8 +15,60 @@ public class TileTest {
     }
 
     @Test
-    public void sampleTest() {
+    //prevents creating a Tile to a position if there already exists a Tile for it  
+    public void testCreateTileOnSamePositionWithDifferentType() {
         Tile overwriteTile = Tile.createTile(new Position(0, 0), Type.PASS);
         assertEquals(overwriteTile, this.firstTile);
     }
+
+    @Test
+    public void sameInstanceForSamePosition(){
+        Tile sameTile = Tile.createTile(new Position(0, 0), Type.WALL);
+        assertEquals(sameTile, this.firstTile);
+    }
+
+    @Test
+    public void differentPositions(){
+        Tile newTile = Tile.createTile(new Position(1,1), Type.PASS);
+        assertNotEquals(newTile, this.firstTile);
+    }
+
+    @Test
+    public void testGetPosition(){
+        Position p = new Position(0,0);
+        assertEquals(p, this.firstTile.getPosition());
+    }
+
+    @Test
+    public void testGetType(){
+        Type t = Type.WALL;
+        assertEquals(t, this.firstTile.getType());
+    }
+
+    @Test
+    public void testValidPath(){
+        Tile passTile = Tile.createTile(new Position(1,1), Type.PASS);
+        assertEquals(passTile.validPath(), true);
+
+        Tile failTile = Tile.createTile(new Position(2,2), Type.WALL);
+        assertEquals(failTile.validPath(), false);
+    }
+
+    @Test
+    public void testNullType(){
+        Tile bothParamNullTile = Tile.createTile(null, null);
+        assertEquals(null, bothParamNullTile.getPosition());
+
+        assertEquals(null, bothParamNullTile.getType());
+
+        Tile positionNullTile = Tile.createTile(null, Type.PASS);
+        assertEquals(null, positionNullTile.getPosition());
+        assertEquals(null, positionNullTile.getType());
+
+        Position p = new Position(2,2);
+        Tile typeNullTile = Tile.createTile(new Position(2,2), null);
+        assertEquals(p, typeNullTile.getPosition());
+        assertEquals(null, typeNullTile.getType());
+    }
+
 }
